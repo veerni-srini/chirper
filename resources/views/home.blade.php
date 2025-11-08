ee90984956179106f75a6ec33dff1e2869ec9806
Srinivas <veerni.srini@gmail.com>
2025-11-09 02:14:41 +0530

    Chirp Controller with Resource

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,18 @@
-<div>
-    <!-- People find pleasure in different ways. I find it in keeping my mind clear. - Marcus Aurelius -->
-</div>
+<x-layout>
+    <x-slot:title>
+        Welcome
+    </x-slot:title>
+    <div class="max-w-2xl mx-auto">
+        @foreach ($chirps as $chirp)
+            <div class="card bg-base-100 shadow mt-8">
+                <div class="card-body">
+                    <div>
+                        <div class="font-semibold">{{ $chirp['author'] }}</div>
+                        <div class="mt-1">{{ $chirp['message'] }}</div>
+                        <div class="text-sm text-gray-500 mt-2">{{ $chirp['time'] }}</div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ChirpController::class, 'index']);
