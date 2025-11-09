312e290c6805ecd69bb15c8f7ac500d7ef61c4bb
Srinivas <veerni.srini@gmail.com>
2025-11-09 17:45:15 +0530

    Displaying all Chirps

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,16 +3,18 @@
         Welcome
     </x-slot:title>
     <div class="max-w-2xl mx-auto">
-        @foreach ($chirps as $chirp)
+        @forelse ($chirps as $chirp)
             <div class="card bg-base-100 shadow mt-8">
                 <div class="card-body">
                     <div>
-                        <div class="font-semibold">{{ $chirp['author'] }}</div>
-                        <div class="mt-1">{{ $chirp['message'] }}</div>
-                        <div class="text-sm text-gray-500 mt-2">{{ $chirp['time'] }}</div>
+                        <div class="font-semibold">{{ $chirp->user ? $chirp->user->name : 'Anonymous' }}</div>
+                        <div class="mt-1">{{ $chirp->message }}</div>
+                        <div class="text-sm text-gray-500 mt-2">{{ $chirp->created_at->diffForHumans() }}</div>
                     </div>
                 </div>
             </div>
-        @endforeach
+        @empty
+            <p class="text-gray-500">No chirps yet. Be the first to chirp!</p>
+        @endforelse
     </div>
 </x-layout>
